b2cd2c022ff3e27058d7aab89f583ec5d4a64dc0
Shloka Jha <shlokajha05@gmail.com>
2024-07-22 11:32:00 -0400

    added day2.js file

---
+++ day2.js
@@ -0,0 +1,73 @@
+//Activity 1//
+let a = 5;
+let b = 3;
+console.log("Sum:", a + b);
+console.log("Difference:", a- b);
+console.log("Product:", a * b);
+console.log("Quotient:", a / b);
+console.log("Remainder:", a % b);
+//Activity 2//
+let k = 10;
+k += 5;
+console.log("After += 5:", k);
+let l = 10;
+l -= 3;
+console.log("After -= 3:", l);
+let m = 8;
+let n = 12;
+//Activity 3//
+console.log(" m > n?", m > n);
+console.log(" m < n?", m < n);
+console.log(" m >= n?", m >= n);
+console.log(" m<= n?", m <= n);
+let q = '5';
+let r = 5;
+console.log(" q == r?", q == r);  // true
+console.log(" q === r?", q === r);  // false
+//Activity 4//
+var ab1=15;
+var ab2=25;
+if(ab1>10 && ab2>10){
+    console.log("Both conditons are satisfied")
+}else{
+    console.log("Both conditions are not satisfied")
+}
+if(ab1>10 || ab2>10){
+    console.log("Atleast one conditon is satisfied")
+}else{
+    console.log("Both conditions are not satisfied")
+}
+if(!(ab2>ab1)){
+    console.log("ab1 is greater");
+}else{
+    console.log("ab2 is greater");
+}
+//Activity 5
+
+let x = -10;
+let result = x >= 0 ? "Positive" : "Negative";
+console.log("The number is:", result);
+
+///Feature Request///
+//Arithmetic Operations Script://
+let num1 = 8;
+let num2 = 3;
+console.log("Addition:", num1 + num2);
+console.log("Subtraction:", num1 - num2);
+console.log("Multiplication:", num1 * num2);
+console.log("Division:", num1 / num2);
+console.log("Remainder:", num1 % num2);
+//Comparison and Logical Operators Script://
+let a1 = 23;
+let b1 = 32;
+console.log("a1 > b1:", a1 > b1);
+console.log("a1 < b1:", a1 < b1);
+console.log("a1 >= b1:", a1 >= b1);
+console.log("a1 <= b1:", a1 <= b1);
+console.log("a1 == b1:", a1 == b1);
+console.log("a1 === b1:", a1 === b1);
+//Ternary Operator Script://
+let num = -5;
+let output = num >= 0 ? "Positive" : "Negative";
+console.log("The number is:", output);
+
